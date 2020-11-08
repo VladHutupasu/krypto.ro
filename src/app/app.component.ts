6ee56fd765c0bf063b0c59c8b1ba2ff95cb605d7
VladHutupasu <vlad.hutupasu@yahoo.com>
2020-11-08 13:04:54 +0100

    Add Firebase + favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 
+declare let gtag: Function;
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -11,6 +13,14 @@ export class AppComponent {
   constructor(translate: TranslateService, private router: Router) {
     translate.setDefaultLang("ro");
     translate.use("ro");
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag("config", "G-VMF56FPCH9", {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
   }
 
   ngOnInit() {
